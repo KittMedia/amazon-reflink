633c9b68c5f2494bcd9a540926d4aa68d67b2d28
Matthias Kittsteiner <mk@kittmedia.com>
2020-07-28 18:50:35 +0200

    Fix receiving title tag

--- lib/util/StringUtil.class.php
+++ lib/util/StringUtil.class.php
@@ -33,8 +33,8 @@ class StringUtil {
 		$str = file_get_contents($url);
 		
 		if (strlen($str) > 0){
-			$str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-			preg_match('/<title([^>]*)>([^>]*)<\/title>/i', $str, $title); // ignore case
+			//$str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+			preg_match('/<title([^>]*)>([^>]*)<\/title>/mi', $str, $title); // ignore case
 			
 			if (!empty($title[2])) {
 				return $title[2];
